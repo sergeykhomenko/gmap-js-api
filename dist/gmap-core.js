bb5158fa972744ec98bc7fcdec5afbb62820e174
Sergey Khomenko <khomenko.mail@gmail.com>
2017-09-30 11:31:38 +0300

    Проверка входных данных

--- dist/gmap-core.js
+++ dist/gmap-core.js
@@ -12,6 +12,7 @@ var GMap = function () {
         _classCallCheck(this, GMap);
 
         this.error_log = [];
+
         this.defaults = {
             map_id: 'gmap-container',
             wrong_way: { lat: 49.9916, lng: 36.2319 },
@@ -19,7 +20,6 @@ var GMap = function () {
             zoom: 17,
             switch_class: 'gmap-reroute'
         };
-        this.map_wrapper = mapWrapper;
 
         // Construction errors hadler
         if (!this.checkRequirements.apply(this, arguments)) {
@@ -52,26 +52,6 @@ var GMap = function () {
 
             return false;
         }
-
-        // GMap properties
-        this.options = this.loadOptions(initOptions);
-
-        this.map = false;
-        this.points = points;
-        this.directions = false;
-        this.marker = false;
-        this.customer = false;
-
-        // Load Google Maps API
-        if (!this.loadGmapsApi(mapWrapper)) {
-            console.log('API can not be loaded. Details: ' + this.error_log[0]);
-            return false;
-        }
-
-        // Add buttons
-        if (typeof jQuery !== 'undefined') {
-            this.addJQuerySwitches();
-        }
     }
 
     _createClass(GMap, [{
@@ -94,125 +74,6 @@ var GMap = function () {
 
             return complete;
         }
-    }, {
-        key: 'loadOptions',
-        value: function loadOptions(opt_object) {
-            var show_marker_option = true;
-
-            if (typeof opt_object.show_marker !== 'undefined') show_marker_option = opt_object.show_marker;
-
-            return {
-                map_id: opt_object.map_id || this.defaults.map_id,
-                wrong_way: opt_object.wrong_way || this.defaults.wrong_way,
-                default_point: opt_object.default_point || this.defaults.default_point,
-                zoom: opt_object.zoom || this.defaults.zoom,
-                switch_class: opt_object.switch_class || this.defaults.switch_class,
-                show_marker: show_marker_option
-            };
-        }
-    }, {
-        key: 'loadGmapsApi',
-        value: function loadGmapsApi(variable) {
-            // If api is loaded
-            if (document.getElementById('gmaps-api') !== null) {
-                return true;
-            }
-
-            // Does not exist API key
-            if (typeof google_maps_key == 'undefined') {
-                this.error_log.push('API key is undefined');
-                return false;
-            }
-
-            var api_script = document.createElement('script');
-            api_script.id = 'gmaps-api';
-            api_script.src = 'https://maps.googleapis.com/maps/api/js?key=' + google_maps_key + '&callback=' + variable + '.renderMap';
-
-            document.body.append(api_script);
-            return true;
-        }
-    }, {
-        key: 'renderMap',
-        value: function renderMap() {
-            var map_element = document.createElement('div');
-            map_element.id = this.options.map_id;
-            document.getElementById(this.map_wrapper).append(map_element);
-
-            var map_wrapper = document.getElementById(this.options.map_id);
-            this.map = new google.maps.Map(map_wrapper, {
-                center: this.points[this.options.default_point].pos,
-                zoom: this.options.zoom,
-                scrollwheel: false
-            });
-
-            this.directions = new google.maps.DirectionsRenderer({
-                preserveViewport: true // no zoom when render, helps on mobile
-            });
-
-            if (this.options.show_marker) {
-                this.marker = new google.maps.InfoWindow({
-                    map: this.map,
-                    position: this.points[this.options.default_point].pos,
-                    content: this.points[this.options.default_point].name
-                });
-            }
-
-            // HTML5 geolocation
-            var location = navigator.geolocation;
-            var customer = void 0;
-            if (location) {
-                location.getCurrentPosition(function (position) {
-                    customer = {
-                        lat: position.coords.latitude,
-                        lng: position.coords.longitude
-                    };
-                }, function () {
-                    customer = this.locationError();
-                });
-            } else {
-                customer = this.locationError();
-            }
-
-            this.route(customer, this.points[this.options.default_point]);
-        }
-    }, {
-        key: 'locationError',
-        value: function locationError() {
-            return this.options.wrong_way;
-        }
-    }, {
-        key: 'route',
-        value: function route(customer_position, destination_point) {
-            var direction_service = new google.maps.DirectionsService();
-            var direction_request = {
-                origin: customer_position,
-                destination: destination_point.pos,
-                travelMode: google.maps.TravelMode.WALKING
-            };
-
-            direction_service.route(direction_request, this.routeCallback);
-            if (this.marker) {
-                this.marker.setContent(destination_point.name);
-                this.marker.setPosition(destination_point.pos);
-            }
-
-            console.log(customer_position);
-            this.map.setCenter({ lat: customer_position.lat, lng: customer_position.lng });
-        }
-    }, {
-        key: 'routeCallback',
-        value: function routeCallback(response, status) {
-            if (status === 'OK') {
-                this.directions.setMap(this.map);
-                this.directions.setDirections(response);
-            }
-        }
-    }, {
-        key: 'rerouter',
-        value: function rerouter(points_iterator) {}
-    }, {
-        key: 'addJQuerySwitches',
-        value: function addJQuerySwitches() {}
     }]);
 
     return GMap;
--- src/gmap-core.js
+++ src/gmap-core.js
@@ -1,7 +1,9 @@
 class GMap {
 
     constructor(mapWrapper, points = [], initOptions = {}) {
+
         this.error_log = [];
+
         this.defaults = {
             map_id: 'gmap-container',
             wrong_way: { lat: 49.9916, lng: 36.2319 },
@@ -9,7 +11,6 @@ class GMap {
             zoom: 17,
             switch_class: 'gmap-reroute'
         };
-        this.map_wrapper = mapWrapper;
 
         // Construction errors hadler
         if (!this.checkRequirements(...arguments)) {
@@ -20,25 +21,6 @@ class GMap {
             return false;
         }
 
-        // GMap properties
-        this.options = this.loadOptions(initOptions);
-
-        this.map = false;
-        this.points = points;
-        this.directions = false;
-        this.marker = false;
-        this.customer = false;
-
-        // Load Google Maps API
-        if (!this.loadGmapsApi(mapWrapper)) {
-            console.log(`API can not be loaded. Details: ${this.error_log[0]}`);
-            return false;
-        }
-
-        // Add buttons
-        if (typeof jQuery !== 'undefined') {
-            this.addJQuerySwitches();
-        }
     }
 
     checkRequirements(mapWrapper, points = [], initOptions = {}) {
@@ -55,126 +37,6 @@ class GMap {
         }
 
         return complete;
-    }
-
-    loadOptions(opt_object) {
-        let show_marker_option = true;
-
-        if (typeof opt_object.show_marker !== 'undefined')
-            show_marker_option = opt_object.show_marker;
-
-        return {
-            map_id: opt_object.map_id || this.defaults.map_id,
-            wrong_way: opt_object.wrong_way || this.defaults.wrong_way,
-            default_point: opt_object.default_point || this.defaults.default_point,
-            zoom: opt_object.zoom || this.defaults.zoom,
-            switch_class: opt_object.switch_class || this.defaults.switch_class,
-            show_marker: show_marker_option
-        };
-    }
-
-    loadGmapsApi(variable) {
-        // If api is loaded
-        if (document.getElementById('gmaps-api') !== null) {
-            return true;
-        }
-
-        // Does not exist API key
-        if (typeof google_maps_key == 'undefined') {
-            this.error_log.push('API key is undefined');
-            return false;
-        }
-
-        let api_script = document.createElement('script');
-        api_script.id = 'gmaps-api';
-        api_script.src = `https://maps.googleapis.com/maps/api/js?key=${google_maps_key}&callback=${variable}.renderMap`;
-
-        document.body.append(api_script);
-        return true;
-    }
-
-    renderMap() {
-        let map_element = document.createElement('div');
-        map_element.id = this.options.map_id;
-        document.getElementById(this.map_wrapper).append(map_element);
-
-        let map_wrapper = document.getElementById(this.options.map_id);
-        this.map = new google.maps.Map(
-            map_wrapper,
-            {
-                center: this.points[this.options.default_point].pos,
-                zoom: this.options.zoom,
-                scrollwheel: false
-            }
-        );
-
-        this.directions = new google.maps.DirectionsRenderer({
-            preserveViewport: true // no zoom when render, helps on mobile
-        });
-
-        if (this.options.show_marker) {
-            this.marker = new google.maps.InfoWindow({
-                map: this.map,
-                position: this.points[this.options.default_point].pos,
-                content: this.points[this.options.default_point].name
-            });
-        }
-
-        // HTML5 geolocation
-        let location = navigator.geolocation;
-        let customer;
-        if (location) {
-            location.getCurrentPosition(function (position) {
-                customer = {
-                    lat: position.coords.latitude,
-                    lng: position.coords.longitude
-                };
-            }, function () {
-                customer = this.locationError();
-            });
-        } else {
-            customer = this.locationError();
-        }
-
-        this.route(customer, this.points[this.options.default_point]);
-
-    }
-
-    locationError() {
-        return this.options.wrong_way;
-    }
-
-    route(customer_position, destination_point) {
-        let direction_service = new google.maps.DirectionsService();
-        let direction_request = {
-            origin: customer_position,
-            destination: destination_point.pos,
-            travelMode: google.maps.TravelMode.WALKING
-        };
-
-        direction_service.route(direction_request, this.routeCallback);
-        if (this.marker) {
-            this.marker.setContent(destination_point.name);
-            this.marker.setPosition(destination_point.pos);
-        }
-
-        console.log(customer_position);
-        this.map.setCenter({ lat: customer_position.lat, lng: customer_position.lng });
-    }
-
-    routeCallback(response, status) {
-        if (status === 'OK') {
-            this.directions.setMap(this.map)
-            this.directions.setDirections(response);
-        }
-    }
-
-    rerouter(points_iterator) {
-
-    }
-
-    addJQuerySwitches() {
-
     }
 
 }
